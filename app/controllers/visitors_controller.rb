960cd259ff2ee2ba33c345583bc0257445cfc0c1
Alex Salom <alex.salom@gmail.com>
2015-08-20 15:15:53 +0200

    new layouts

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,11 @@
 class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
+    if params[:hola] then
+      flash.now[:notice] = "This is a notice!"
+      flash.now[:alert] = "This is an alert"
+      flash.now[:warning] = "This is an warning"
+    end
     # render 'visitors/new' # -> Implemented in superclass
   end
 end
