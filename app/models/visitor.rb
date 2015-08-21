464c860abaa7e3b0f85df95001e9b9995b3b2d87
Alex Salom <alex.salom@gmail.com>
2015-08-21 14:51:36 +0200

    mailchimp API v3.0 fix

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -6,13 +6,13 @@ class Visitor
 
   def subscribe
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-    result = mailchimp.lists.subscribe({
-                                           :id => Rails.application.secrets.mailchimp_list_id,
-                                           :email => {:email => self.email},
-                                           :double_optin => false,
-                                           :update_existing => true,
-                                           :send_welcome => true
-                                       })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    mailchimp_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(mailchimp_id).members.create(
+                                                          body: {
+                                                              :email_address => self.email,
+                                                              :status => 'subscribed'
+                                                          }
+    )
+    Rails.logger.info("Subscribed #{self.email} to MailChimp.") if result
   end
 end
